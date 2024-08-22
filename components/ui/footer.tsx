be1b3e3d41131224ae7509fe6fe085e84244bfc7
Alejandro Rama <alejandronrf@gmail.com>
2024-08-22 01:59:32 -0300

    ocultar popup & logo oscuro

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 
-import Logo from '@/public/images/LogoJuani.png'
+import Logo from '@/public/images/LogoJuaniOscuro.png'
 
 export default function Footer() {
   return (
