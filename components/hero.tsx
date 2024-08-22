3f0de4e979080ff3fefe5b451147d32184c88bcd
Alejandro Rama <alejandronrf@gmail.com>
2024-08-22 01:30:42 -0300

    features id

--- components/hero.tsx
+++ components/hero.tsx
@@ -28,7 +28,7 @@ export default function Hero() {
             <p className="text-xl text-gray-400 mb-8" data-aos="fade-up" data-aos-delay="200">Motosierra, baja de impuestos, y subtes: Transformando la Ciudad Autónoma de Buenos Aires con propuestas concretas y eficientes.</p>
             <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center">
               <div data-aos="fade-up" data-aos-delay="400">
-                <a className="btn text-white bg-purple-600 hover:bg-purple-700 w-full mb-4 sm:w-auto sm:mb-0" href="#propuestas">Ver propuestas</a>
+                <a className="btn text-white bg-purple-600 hover:bg-purple-700 w-full mb-4 sm:w-auto sm:mb-0" href="#features">Ver propuestas</a>
               </div>
               <div data-aos="fade-up" data-aos-delay="600">
                 <a className="btn text-white bg-gray-700 hover:bg-gray-800 w-full sm:w-auto sm:ml-4" href="#contacto">Súmate al equipo</a>
