88d827e2d805d187da80a437720514b5a6155c47
Alejandro Rama <alejandronrf@gmail.com>
2024-08-22 02:45:39 -0300

    Cambio en ejes

--- components/features.tsx
+++ components/features.tsx
@@ -23,68 +23,44 @@ export default function Features() {
             </div>
 
             {/* 2nd item */}
-            <div className="relative flex flex-col items-center" data-aos="fade-up" data-aos-delay="100" data-aos-anchor="[data-aos-id-blocks]">
-              <svg className="w-16 h-16 mb-4" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
-                <circle className="fill-current text-purple-600" cx="32" cy="32" r="32" />
-                <path className="stroke-current text-purple-100" strokeWidth="2" strokeLinecap="square" d="M21 23h22v18H21z" fill="none" fillRule="evenodd" />
-                <path className="stroke-current text-purple-300" d="M26 28h12M26 32h12M26 36h5" strokeWidth="2" strokeLinecap="square" />
-              </svg>
-              <h4 className="h4 mb-2">Reducción de Gastos Innecesarios</h4>
-              <p className="text-lg text-gray-400 text-center">Aplicaremos la motosierra presupuestaria para eliminar gastos espurios y redirigir fondos a proyectos prioritarios para la ciudad.</p>
+            <div className="relative flex flex-col items-center" data-aos="fade-up" data-aos-anchor="[data-aos-id-blocks]">
+              <div className="w-16 h-16 mb-4 flex items-center justify-center bg-purple-600 rounded-full text-white text-2xl">
+                2
+              </div>
+              <h4 className="h4 mb-2 text-gray-400">Transformación y expansión de la Red de <span className="h4 mb-2 text-purple-600">Subtes</span>:</h4>
+              <ol className="text-lg text-gray-400 text-left list-decimal ml-5">
+                <li>Automatización de Líneas C y D</li>
+                <li>Extensión de Línea H hasta FFCC Belgrano Sur</li>
+                <li>Extensión de la Línea D hasta Puente Saavedra</li>
+                <li>Licitación y Tramo 1 de “Línea I” la cual terminará uniendo 5 líneas de FFCC, 4 Líneas de Subte (A, B, D y E) y Premetro (P)</li>
+              </ol>
             </div>
 
             {/* 3rd item */}
-            <div className="relative flex flex-col items-center" data-aos="fade-up" data-aos-delay="200" data-aos-anchor="[data-aos-id-blocks]">
-              <svg className="w-16 h-16 mb-4" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
-                <rect className="fill-current text-purple-600" width="64" height="64" rx="32" />
-                <g transform="translate(21 21)" strokeLinecap="square" strokeWidth="2" fill="none" fillRule="evenodd">
-                  <ellipse className="stroke-current text-purple-300" cx="11" cy="11" rx="5.5" ry="11" />
-                  <path className="stroke-current text-purple-100" d="M11 0v22M0 11h22" />
-                  <circle className="stroke-current text-purple-100" cx="11" cy="11" r="11" />
-                </g>
-              </svg>
-              <h4 className="h4 mb-2">Baja de Impuestos</h4>
-              <p className="text-lg text-gray-400 text-center">Implementaremos una política de reducción de impuestos para aliviar la carga fiscal de los ciudadanos y fomentar el crecimiento económico.</p>
+            <div className="relative flex flex-col items-center" data-aos="fade-up" data-aos-anchor="[data-aos-id-blocks]">
+              <div className="w-16 h-16 mb-4 flex items-center justify-center bg-purple-600 rounded-full text-white text-2xl">
+                3
+              </div>
+              <h4 className="h4 mb-2 text-purple-600">Ciudad <span className="h4 mb-2 text-gray-400">Sin Barreras</span>:</h4>
+              <p className="text-lg text-gray-400 text-center">Eliminación de los 38 Pasos a Nivel Restantes en la Ciudad de Buenos Aires</p>
             </div>
 
             {/* 4th item */}
-            <div className="relative flex flex-col items-center" data-aos="fade-up" data-aos-delay="300" data-aos-anchor="[data-aos-id-blocks]">
-              <svg className="w-16 h-16 mb-4" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
-                <rect className="fill-current text-purple-600" width="64" height="64" rx="32" />
-                <g transform="translate(22 21)" strokeLinecap="square" strokeWidth="2" fill="none" fillRule="evenodd">
-                  <path className="stroke-current text-purple-100" d="M17 22v-6.3a8.97 8.97 0 003-6.569A9.1 9.1 0 0011.262 0 9 9 0 002 9v1l-2 5 2 1v4a2 2 0 002 2h4a5 5 0 005-5v-5" />
-                  <circle className="stroke-current text-purple-300" cx="13" cy="9" r="3" />
-                </g>
-              </svg>
-              <h4 className="h4 mb-2">Mejora de Infraestructura Urbana</h4>
-              <p className="text-lg text-gray-400 text-center">Implementaremos un plan integral para mejorar veredas, construir pasos bajo nivel y optimizar la infraestructura de la ciudad.</p>
+            <div className="relative flex flex-col items-center" data-aos="fade-up" data-aos-anchor="[data-aos-id-blocks]">
+              <div className="w-16 h-16 mb-4 flex items-center justify-center bg-purple-600 rounded-full text-white text-2xl">
+                4
+              </div>
+              <h4 className="h4 mb-2 text-purple-600">Eliminación de <span className="h4 mb-2 text-gray-400">Bicisendas espurias</span>:</h4>
+              <p className="text-lg text-gray-400 text-center">Eliminación progresiva de las bicisendas no utilizadas y habilitación de estacionamiento en ambos lados</p>
             </div>
 
             {/* 5th item */}
-            <div className="relative flex flex-col items-center" data-aos="fade-up" data-aos-delay="400" data-aos-anchor="[data-aos-id-blocks]">
-              <svg className="w-16 h-16 mb-4" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
-                <rect className="fill-current text-purple-600" width="64" height="64" rx="32" />
-                <g strokeLinecap="square" strokeWidth="2" fill="none" fillRule="evenodd">
-                  <path className="stroke-current text-purple-100" d="M29 42h10.229a2 2 0 001.912-1.412l2.769-9A2 2 0 0042 29h-7v-4c0-2.373-1.251-3.494-2.764-3.86a1.006 1.006 0 00-1.236.979V26l-5 6" />
-                  <path className="stroke-current text-purple-300" d="M22 30h4v12h-4z" />
-                </g>
-              </svg>
-              <h4 className="h4 mb-2">Eliminación de Políticas Ineficientes</h4>
-              <p className="text-lg text-gray-400 text-center">Revisaremos y eliminaremos políticas ineficientes, como la VTV, y optimizaremos la gestión de la ciudad para mayor eficiencia.</p>
-            </div>
-
-            {/* 6th item */}
-            <div className="relative flex flex-col items-center" data-aos="fade-up" data-aos-delay="500" data-aos-anchor="[data-aos-id-blocks]">
-              <svg className="w-16 h-16 mb-4" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
-                <rect className="fill-current text-purple-600" width="64" height="64" rx="32" />
-                <g transform="translate(21 22)" strokeLinecap="square" strokeWidth="2" fill="none" fillRule="evenodd">
-                  <path className="stroke-current text-purple-300" d="M17 2V0M19.121 2.879l1.415-1.415M20 5h2M19.121 7.121l1.415 1.415M17 8v2M14.879 7.121l-1.415 1.415M14 5h-2M14.879 2.879l-1.415-1.415" />
-                  <circle className="stroke-current text-purple-300" cx="17" cy="5" r="3" />
-                  <path className="stroke-current text-purple-100" d="M8.86 1.18C3.8 1.988 0 5.6 0 10c0 5 4.9 9 11 9a10.55 10.55 0 003.1-.4L20 21l-.6-5.2a9.125 9.125 0 001.991-2.948" />
-                </g>
-              </svg>
-              <h4 className="h4 mb-2">Transparencia y Participación Ciudadana</h4>
-              <p className="text-lg text-gray-400 text-center">Implementaremos medidas para aumentar la transparencia en la gestión y fomentar la participación activa de los ciudadanos en las decisiones de la ciudad.</p>
+            <div className="relative flex flex-col items-center" data-aos="fade-up" data-aos-anchor="[data-aos-id-blocks]">
+              <div className="w-16 h-16 mb-4 flex items-center justify-center bg-purple-600 rounded-full text-white text-2xl">
+                5
+              </div>
+              <h4 className="h4 mb-2 text-purple-600">Veredas <span className="h4 mb-2 text-gray-400">modernas, funcionales y costo-eficientes</span>:</h4>
+              <p className="text-lg text-gray-400 text-center">Desarrollo de veredas de cemento alisado y renovación progresiva de veredas en estado deficiente</p>
             </div>
 
           </div>
